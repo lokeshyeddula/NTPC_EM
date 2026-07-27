51fd3f41f568cf06d56b3f064faf1d0e165f8ebb
YeddulaLokesh <lokeshyeddula.11@gmail.com>
2026-07-27 15:55:52 +0530

    Add health endpoint

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,36 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import health
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
 
-    path(
-        "admin/",
-        admin.site.urls,
-    ),
+    path("", health),
 
-    path(
-        "api/auth/",
-        include("accounts.urls"),
-    ),
+    path("admin/", admin.site.urls),
 
-    path(
-        "api/machinery/",
-        include("machinery.urls"),
-    ),
+    path("api/auth/", include("accounts.urls")),
 
-    path(
-        "api/inspections/",
-        include("inspections.urls"),
-    ),
+    path("api/machinery/", include("machinery.urls")),
 
-    path(
-        "api/reports/",
-        include("reporting.urls"),
-    ),
+    path("api/inspections/", include("inspections.urls")),
 
+    path("api/reports/", include("reporting.urls")),
 ]
 
 if settings.DEBUG:
