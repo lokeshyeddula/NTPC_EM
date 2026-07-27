9baa40a6953677e8cc3b627acc82847e9e8a45f1
YeddulaLokesh <lokeshyeddula.11@gmail.com>
2026-07-27 20:54:19 +0530

    27/07/26

--- backend/machinery/views.py
+++ backend/machinery/views.py
@@ -1,41 +1,28 @@
 from rest_framework import viewsets
+from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import MachineryType, Vehicle
-from .serializers import (
-    MachineryTypeSerializer,
-    VehicleSerializer,
-)
-from rest_framework.generics import ListAPIView
+from .serializers import MachineryTypeSerializer, VehicleSerializer
 
-from .serializers import VehicleSerializer
-
-
-from rest_framework.generics import ListAPIView
-from rest_framework.permissions import IsAuthenticated
-
-from .models import Vehicle
-from .serializers import VehicleSerializer
 
 class VehicleByMachineAPIView(ListAPIView):
-
-    serializer_class = VehicleSerializer
     permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
-
+    def list(self, request, *args, **kwargs):
         machinery_type_id = self.kwargs["machinery_type_id"]
 
-        # FIXED: Added select_related to prevent N+1 queries
-        # FIXED: Added defer('remarks') to save massive amounts of RAM
-        return Vehicle.objects.select_related(
-            "machinery_type"
-        ).filter(
+        # Fetch ONLY 'id' and 'machine_number' as lightweight dictionaries.
+        # This completely bypasses the memory-heavy VehicleSerializer and prevents OOM crashes.
+        vehicles = Vehicle.objects.filter(
             machinery_type_id=machinery_type_id,
             status="Active",
-        ).defer(
-            "remarks"
-        ).order_by("machine_number")
+        ).values("id", "machine_number").order_by("machine_number")
+
+        return Response(list(vehicles))
+
+
 class MachineryTypeViewSet(viewsets.ModelViewSet):
     queryset = MachineryType.objects.filter(
         name__in=[
@@ -55,16 +42,13 @@ class MachineryTypeViewSet(viewsets.ModelViewSet):
     ).order_by("name")
 
     serializer_class = MachineryTypeSerializer
-
     permission_classes = [IsAuthenticated]
 
 
 class VehicleViewSet(viewsets.ModelViewSet):
-
     queryset = Vehicle.objects.select_related(
         "machinery_type"
     )
 
     serializer_class = VehicleSerializer
-
     permission_classes = [IsAuthenticated]
